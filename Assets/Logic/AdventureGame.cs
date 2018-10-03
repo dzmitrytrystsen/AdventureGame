d42a25bdcd67c95f82c80719da66f55e5f1852d0
dzmitrytrystsen <tristendmitry@gmail.com>
2018-10-03 11:51:25 +0300

    AdventureGame
    AdventureGame

---
+++ Assets/Logic/AdventureGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+	public void Start ()
+	{
+	    state = startingState;
+	    textComponent.text = state.GetStateStory();
+	}
+
+	public void Update ()
+	{
+	    ManageStates();
+	}
+
+    public void ManageStates()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+                state = nextStates[i];
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
